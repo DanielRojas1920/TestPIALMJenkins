9b0892f86820aaee7658b53e447e610e8cecee8b
DanielRojas1920 <danielrojvilla@gmail.com>
2024-11-07 01:32:32 -0600

    docker&terraform

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Usa la imagen base oficial de Python
+FROM python:3.9
+
+# Configura el directorio de trabajo en el contenedor
+WORKDIR /app
+
+# Copia el archivo de requisitos para instalar dependencias
+COPY requirements.txt /app/
+
+# Instala las dependencias
+RUN pip install -r requirements.txt
+
+# Copia el resto del código del proyecto
+COPY . /app/
+
+# Expone el puerto para el servidor Django
+EXPOSE 8000
---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Imagen y contenedor para la aplicación Django
+resource "docker_image" "django_app" {
+  name = "django_app"
+  build {
+    context    = "."
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_container" "django_container" {
+  image = docker_image.django_app.latest
+  name  = "django_container"
+  ports {
+    internal = 8000
+    external = 8000
+  }
+
+  command = ["sh", "-c", "python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
+  depends_on = [docker_container.mysql_container]
+}
+
+# Imagen y contenedor para MySQL
+resource "docker_container" "mysql_container" {
+  name    = "mysql_database"
+  image   = "mysql:5.7"  # Puedes usar una versión diferente si es necesario
+  env = [
+    "MYSQL_ROOT_PASSWORD=9WG\"\"u4:85M",
+    "MYSQL_DATABASE=PIA_LMP",
+    "MYSQL_USER=PIA_LMP",
+    "MYSQL_PASSWORD=9WG\"\"u4:85M"
+  ]
+  ports {
+    internal = 3306
+    external = 3306
+  }
+}
